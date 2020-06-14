8d87f7cd66551ea229367bd8dc41f2e37a12e374
Arun Kumar M <marunkumar664@gmail.com>
2020-06-14 19:26:00 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- express-generator/confusionserver/app.js
+++ express-generator/confusionserver/app.js
@@ -10,6 +10,15 @@ var dishRouter = require('./routes/dishRouter');
 var leadRouter = require('./routes/leadRouter');
 var promRouter = require('./routes/promRouter');
 
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 
 var app = express();
 
