910c58aa88c4f8bb94922f02494addbbb9417573
Arun Kumar M <marunkumar664@gmail.com>
2020-06-25 16:40:34 +0530

    File Upload

--- express-generator/confusionserver/app.js
+++ express-generator/confusionserver/app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leadRouter = require('./routes/leadRouter');
 var promRouter = require('./routes/promRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -84,6 +85,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/leaders', leadRouter);
 app.use('/promotions', promRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
