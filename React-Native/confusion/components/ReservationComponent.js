0a4fe7afc9724f64e99a1e7412665719a0c85485
Arun Kumar M <marunkumar664@gmail.com>
2020-09-07 22:36:59 +0530

    Local Notifications

--- React-Native/confusion/components/ReservationComponent.js
+++ React-Native/confusion/components/ReservationComponent.js
@@ -2,112 +2,160 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, ScrollView, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            guests: 1,
-            smoking: false,
-            date: ''
-        }
-        this.resetForm = this.resetForm.bind(this);
-    }
-
-    static navigationOptions = {
-        title: 'Reserve Table'
+  constructor(props) {
+    super(props);
+    this.state = {
+      guests: 1,
+      smoking: false,
+      date: "",
     };
+    this.resetForm = this.resetForm.bind(this);
+    this.presentLocalNotification = this.presentLocalNotification.bind(this);
+  }
 
-    handleReservation() {
-        Alert.alert(
-            'Your Reservation OK ?',
-            'Number of Guests'+this.state.guests+'\n'+
-            'Smoking?'+this.state.smoking+'\n'+
-            'Date and Time: '+this.state.date,
-            [
-            {text: 'Cancel', onPress: () => {this.resetForm()}},
-            {text: 'OK', onPress: () => {this.resetForm()}},
-            ],
-            { cancelable: false }
-        );
-    }
+  static navigationOptions = {
+    title: "Reserve Table",
+  };
 
-    resetForm() {
-        this.setState({
-            guests: 1,
-            smoking: false,
-            date: '',
-            showModal: false
-        });
-    }
-    
-    render() {
-        return(
-            <ScrollView>
-                <Animatable.View animation="zoomIn" duration={1000} delay={500}>
-                <View style={styles.formRow}>
-                <Text style={styles.formLabel}>Number of Guests</Text>
-                <Picker
-                    style={styles.formItem}
-                    selectedValue={this.state.guests}
-                    onValueChange={(itemValue, itemIndex) => this.setState({guests: itemValue})}>
-                    <Picker.Item label="1" value="1" />
-                    <Picker.Item label="2" value="2" />
-                    <Picker.Item label="3" value="3" />
-                    <Picker.Item label="4" value="4" />
-                    <Picker.Item label="5" value="5" />
-                    <Picker.Item label="6" value="6" />
-                </Picker>
-                </View>
-                <View style={styles.formRow}>
-                <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
-                <Switch
-                    style={styles.formItem}
-                    value={this.state.smoking}
-                    trackColor='#512DA8'
-                    onValueChange={(value) => this.setState({smoking: value})}>
-                </Switch>
-                </View>
-                <View style={styles.formRow}>
-                <Text style={styles.formLabel}>Date and Time</Text>
-                <DatePicker
-                    style={{flex: 2, marginRight: 20}}
-                    date={this.state.date}
-                    format=''
-                    mode="date"
-                    placeholder="select date and Time"
-                    minDate="2017-01-01"
-                    confirmBtnText="Confirm"
-                    cancelBtnText="Cancel"
-                    customStyles={{
-                    dateIcon: {
-                        position: 'absolute',
-                        left: 0,
-                        top: 4,
-                        marginLeft: 0
-                    },
-                    dateInput: {
-                        marginLeft: 36
-                    }
-                    // ... You can check the source to find the other keys. 
-                    }}
-                    onDateChange={(date) => {this.setState({date: date})}}
-                />
-                </View>
-                <View style={styles.formRow}>
-                <Button
-                    onPress={() => this.handleReservation()}
-                    title="Reserve"
-                    color="#512DA8"
-                    accessibilityLabel="Learn more about this purple button"
-                    />
-                </View>
-                </Animatable.View>
-            </ScrollView>
-        );
-    }
+  async obtainNotificationPermission() {
+    const { status, expires, permissions } = await Permissions.getAsync(
+        Permissions.USER_FACING_NOTIFICATIONS
+      );
+      if (status !== 'granted') {
+        alert('Hey! You have not enabled selected permissions');
+      }
+      return permissions;
+}
 
+async presentLocalNotification(date) {
+    await this.obtainNotificationPermission();
+    Notifications.presentNotificationAsync({
+        title: 'Your Reservation',
+        body: 'Reservation for '+ date + ' requested',
+        ios: {
+            sound: true
+        },
+        android: {
+            sound: true,
+            vibrate: true,
+            color: '#512DA8'
+        }
+    });
+}
+
+  handleReservation() {
+    Alert.alert(
+      "Your Reservation OK ?",
+      "Number of Guests" +
+        this.state.guests +
+        "\n" +
+        "Smoking?" +
+        this.state.smoking +
+        "\n" +
+        "Date and Time: " +
+        this.state.date,
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            this.resetForm();
+          },
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            this.presentLocalNotification(this.state.date);
+            this.resetForm();
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+
+  resetForm() {
+    this.setState({
+      guests: 1,
+      smoking: false,
+      date: "",
+      showModal: false,
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="zoomIn" duration={1000} delay={500}>
+          <View style={styles.formRow}>
+            <Text style={styles.formLabel}>Number of Guests</Text>
+            <Picker
+              style={styles.formItem}
+              selectedValue={this.state.guests}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ guests: itemValue })
+              }
+            >
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5" />
+              <Picker.Item label="6" value="6" />
+            </Picker>
+          </View>
+          <View style={styles.formRow}>
+            <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
+            <Switch
+              style={styles.formItem}
+              value={this.state.smoking}
+              trackColor="#512DA8"
+              onValueChange={(value) => this.setState({ smoking: value })}
+            ></Switch>
+          </View>
+          <View style={styles.formRow}>
+            <Text style={styles.formLabel}>Date and Time</Text>
+            <DatePicker
+              style={{ flex: 2, marginRight: 20 }}
+              date={this.state.date}
+              format=""
+              mode="date"
+              placeholder="select date and Time"
+              minDate="2017-01-01"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              customStyles={{
+                dateIcon: {
+                  position: "absolute",
+                  left: 0,
+                  top: 4,
+                  marginLeft: 0,
+                },
+                dateInput: {
+                  marginLeft: 36,
+                },
+                // ... You can check the source to find the other keys.
+              }}
+              onDateChange={(date) => {
+                this.setState({ date: date });
+              }}
+            />
+          </View>
+          <View style={styles.formRow}>
+            <Button
+              onPress={() => this.handleReservation()}
+              title="Reserve"
+              color="#512DA8"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
