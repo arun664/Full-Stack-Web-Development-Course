4bbaf3643372cae907c26dbdee430112a5b1316e
Arun Kumar M <marunkumar664@gmail.com>
2020-09-07 17:48:21 +0530

    Deleting Favorites

--- React-Native/confusion/redux/ActionCreators.js
+++ React-Native/confusion/redux/ActionCreators.js
@@ -193,3 +193,8 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     .then(response => dispatch(addComment(response)))
     .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
 };
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+})
